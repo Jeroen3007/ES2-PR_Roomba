e9f37c2398d8b8d428dcbed060dc7260690fdcc9
Xinq- <jellelolkema@icloud.com>
2016-03-21 09:50:29 +0100

    switch

--- Roomba_Interpreter/failsave.cpp
+++ Roomba_Interpreter/failsave.cpp
@@ -37,7 +37,7 @@ void failsave::checkCodes()
         if(Interpreter->getCliffFrontRightSignal() > 2048)uart->sendUart(Stop);
         if(Interpreter->getCliffRightSignal() > 2048)uart->sendUart(Stop);
         if(Interpreter->getLeftWheelOvercurrent())uart->sendUart(Stop);
-        if((Interpreter->getBatteryCapacity()/Opcodes->getBatteryCharge()*100) < 10)uart->sendUart(Stop);
+        if((Interpreter->getBatteryCapacity()/Interpreter->getBatteryCharge()*100) < 10)uart->sendUart(Stop);
 
         Interpreter->unlockMutex(); // free mutex for standard procedure
     }

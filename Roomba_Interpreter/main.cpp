5b272e647d57dc96fc06b5a9e909992f866f6784
Xinq- <jellelolkema@icloud.com>
2016-03-21 10:07:34 +0100

    switch

--- Roomba_Interpreter/main.cpp
+++ Roomba_Interpreter/main.cpp
@@ -16,5 +16,7 @@ int main(int argc, char *argv[])
 /*   - drive functie controleren        */
 /*   - individuele sensorfuncties       */
 /*   - evt liedje functie               */
+/*   - failsave and interpreter switch  */
+/*          functie                     */
 /*                                      */
 /****************************************/
